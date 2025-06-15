3856651fe59ea4bee8a5c0b3d8019bfb1bed5a0c
firmanaang <pfirmanullah@gmail.com>
2025-06-15 22:01:11 +0700

    first commit

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,71 +1,52 @@
-import { getCsrfToken, signIn, signOut, getSession } from "next-auth/react";
-import type {
-  SIWEVerifyMessageArgs,
-  SIWECreateMessageArgs,
-  SIWESession,
-} from "@reown/appkit-siwe";
-import { createSIWEConfig, formatMessage } from "@reown/appkit-siwe";
-import { mainnet, sepolia } from "@reown/appkit/networks";
+import { type DefaultSession, type NextAuthConfig } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 
-export const siweConfig = createSIWEConfig({
-  getMessageParams: async () => ({
-    domain: typeof window !== "undefined" ? window.location.host : "",
-    uri: typeof window !== "undefined" ? window.location.origin : "",
-    chains: [mainnet.id, sepolia.id],
-    statement: "Please sign with your account",
-  }),
-  createMessage: ({ address, ...args }: SIWECreateMessageArgs) =>
-    formatMessage(args, address),
-  getNonce: async () => {
-    const nonce = await getCsrfToken();
-    if (!nonce) {
-      throw new Error("Failed to get nonce!");
-    }
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
 
-    return nonce;
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authConfig = {
+  providers: [
+    DiscordProvider,
+    /**
+     * ...add more providers here.
+     *
+     * Most other providers require a bit more work than the Discord provider. For example, the
+     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+     *
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub,
+      },
+    }),
   },
-  getSession: async () => {
-    const session = await getSession();
-    if (!session) {
-      return null;
-    }
-
-    // Validate address and chainId types
-    if (
-      typeof session.address !== "string" ||
-      typeof session.chainId !== "number"
-    ) {
-      return null;
-    }
-
-    return {
-      address: session.address,
-      chainId: session.chainId,
-    } satisfies SIWESession;
-  },
-  verifyMessage: async ({ message, signature }: SIWEVerifyMessageArgs) => {
-    try {
-      const success = await signIn("credentials", {
-        message,
-        redirect: false,
-        signature,
-        callbackUrl: "/protected",
-      });
-
-      return Boolean(success?.ok);
-    } catch (error) {
-      return false;
-    }
-  },
-  signOut: async () => {
-    try {
-      await signOut({
-        redirect: false,
-      });
-
-      return true;
-    } catch (error) {
-      return false;
-    }
-  },
-});
+} satisfies NextAuthConfig;
